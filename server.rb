cacaa48e7f3f71a5a5243da1d6a40b1e2f4ff00a
stein328i <stein.328i@gmail.com>
2017-07-12 19:21:31 -0400

    update

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
+
 get '/home' do
 	erb :index
 end
@@ -9,5 +14,14 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+	@skills = ['git', 'HTML', 'CSS', 'Ruby']
+  	@interests = ['cats', 'art', 'music', 'films', 'coffee']
 	erb :about_me
 end
+
+get '/favorite' do
+	@fave_links = ['www.google.com', 'www.bitmaker.co', 'www.utoronto.ca', 'www.york.ca', 'www.yahoo.com']
+	erb :favorite
+end
+
+
